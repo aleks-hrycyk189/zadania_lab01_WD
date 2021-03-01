451b42e32f3ceb276b0a03087c00f1b3a92bf29a
Ola <>
2021-03-01 22:15:09 +0100

    Zadanie domowe nr1

---
+++ main.py
@@ -0,0 +1,114 @@
+# zadanie nr 1
+# number_int = 3
+# numer_float = 548.5
+# c = 'zadanie domowe'
+# d = 'wizualizacja danych'
+# e = True
+# f = False
+#
+# print(number_int)
+# print(numer_float)
+# print(type(number_int))
+# print(type(numer_float))
+# print(c)
+# print(d)
+# print(type(d))
+# print(e)
+# print(f)
+# print(type(e))
+
+
+# zadanie 2
+# x = 10
+# y = 3
+# # str - integer
+#
+# suma = x + y
+# print(suma)
+# print('Dodawanie %d oraz %d to: '%(x,y) + str(suma))
+# print ('Wynik dzialania: %(z1)d + %(z2)d = %(z3)d' %{'z1': x, 'z2': y, 'z3': suma})
+#
+# roznica = x - y
+# print ('Wynik dzialania: %(z1)d - %(z2)d = %(z3)d' %{'z1': x, 'z2': y, 'z3': roznica})
+#
+# iloczyn = x * y
+# print ('Wynik dzialania: %(z1)d * %(z2)d = %(z3)d' %{'z1': x, 'z2': y, 'z3': iloczyn})
+#
+# iloraz = x / y
+# print ('Wynik dzialania: %(z1)d / %(z2)d = %(z3)d' %{'z1': x, 'z2': y, 'z3': iloraz})
+#
+# reszta = x % y
+# print ('Wynik dzialania: %(z1)d %% %(z2)d = %(z3)d' %{'z1': x, 'z2': y, 'z3': reszta})
+#
+# potegowanie = x ** y
+# print ('Wynik dzialania: %(z1)d ** %(z2)d = %(z3)d' %{'z1': x, 'z2': y, 'z3': potegowanie})
+
+
+
+# zadanie 3
+# a = 14
+# b = 2
+# a += b
+# print(a)
+# a = 14
+# b = 2
+# a -= b
+# print(a)
+# a = 14
+# b = 2
+# a *= b
+# print(a)
+# a = 14
+# b = 2
+# a /= b
+# print(a)
+# a = 14
+# b = 2
+# a **= b
+# print(a)
+# a = 14
+# b = 2
+# a %= b
+# print(a)
+
+
+
+# zadanie 4
+# from math import *
+# print(pow(e,10))
+# print(exp(10))
+# z=pow(sin(8),2)
+# d=log(5+z)
+# print(pow(d,(1/6)))
+# print(floor(3.55))
+# print(ceil(4.80))
+
+# zadanie 5
+# imie = 'ALEKSANDRA'
+# nazwisko = 'HRYCYK'
+# print(str.capitalize(imie))
+# print(str.capitalize(nazwisko))
+
+# zadanie 6
+# piosenka = 'laj laj laj dmuchawce, latawce, laj laj laj'
+# print(piosenka.count('laj'))
+
+# zadanie 7
+
+# zm1 = 'Inzynieria systemow informatycznych'
+# print(zm1[1])
+# print(zm1[34])
+
+# zadanie 8
+# piosenka = 'laj laj laj dmuchawce, latawce, laj laj laj'
+# print(piosenka.split(' '))
+
+# zadanie 9
+a = 'Inzynieria systemow informatycznych'
+print(a)
+print(type(a))
+b = 4.5
+print(b)
+print(type(b))
+print('%.20f' % b)
+print('%d' % b)
